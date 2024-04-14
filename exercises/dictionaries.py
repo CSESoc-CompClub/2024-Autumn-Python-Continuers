da994bfdec8ec317d44460b730f29003f184808f
Cindy Wu <madoxxmyst@gmail.com>
2024-04-14 20:15:59 +1000

    dictionary exercise & solution done

---
+++ exercises/dictionaries.py
@@ -0,0 +1,42 @@
+# Dictionaries
+"""
+A student named Jack is using a dictionary to make a list of all the countries he wants to visit in the future,
+starting with their capital cities.
+"""
+capitals = {"USA":"Washington D.C.", "France":"Paris", "China": "Mexico"}
+
+"""1. Jack found a mistake in his dictionary. Can you fix it?"""
+# TODO: IMPLEMENT THIS
+
+
+print('1. Updated dictionary:', capitals)  # DO NOT MODIFY THIS LINE
+
+# =========================================================================================================================#
+"""2. Jack suddenly wants to visit Japan and Spain as well. Can you help him add the relevant entries to his dictionary?"""
+# TODO: IMPLEMENT THIS
+
+print('2. Updated dictionary:', capitals)  # DO NOT MODIFY THIS LINE
+
+# =========================================================================================================================#
+"""3. Jack doesn't want to go to France anymore. Can you update his dictionary to reflect this?"""
+# TODO: IMPLEMENT THIS
+
+print('3. Updated dictionary:', capitals)  # DO NOT MODIFY THIS LINE
+
+# =========================================================================================================================#
+"""4. Edit this for loop so it prints out all the countries and capitals Jack is visiting, and how many cities he visits in total."""
+def countries_and_capitals():
+  # TODO: IMPLEMENT THIS FUNCTION
+  pass
+
+
+countries_and_capitals()
+
+"""
+Expected Output:
+The capital of USA is Washington D.C.
+The capital of China is Beijing
+The capital of Japan is Tokyo
+THe capital of Spain is Madrid
+In the end, Jack visited 4 capital cities.
+"""
