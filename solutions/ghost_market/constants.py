97fc62474c166a9bfd50a73fd9e91447d7ae279e
Vinayak Fialok <z5416144@ad.unsw.edu.au>
2024-04-19 21:52:54 +1000

    run formatter

--- solutions/ghost_market/constants.py
+++ solutions/ghost_market/constants.py
@@ -1,21 +1,22 @@
 CATALOGUE = {
-    'Food': {
-        'Konpeito': 0.5,
-        'Herring and Pumpkin Pie': 20,
-        'Nabeyaki Udon': 12,
-        'Haku\'s Onigiri': 5,
-        'Howl\'s Bacon and Eggs': 10,
-        'Aji Fry': 8.5,
-        'Kiki\'s Chocolate Cake': 17
+    "Food": {
+        "Konpeito": 0.5,
+        "Herring and Pumpkin Pie": 20,
+        "Nabeyaki Udon": 12,
+        "Haku's Onigiri": 5,
+        "Howl's Bacon and Eggs": 10,
+        "Aji Fry": 8.5,
+        "Kiki's Chocolate Cake": 17,
     },
-    'Trinkets': {
-        'Bamboo Fan': 10,
-        'Omamori': 2,
-        'Bright red bow': 5,
-        'Teardrop earrings': 12,
-        'Sturdy Umbrella': 15.5,
+    "Trinkets": {
+        "Bamboo Fan": 10,
+        "Omamori": 2,
+        "Bright red bow": 5,
+        "Teardrop earrings": 12,
+        "Sturdy Umbrella": 15.5,
     },
 }
+
 
 # Returns True if the Item exists in the Catalogue
 # Returns False otherwise
@@ -23,7 +24,8 @@ def is_valid_item(item):
     for category in CATALOGUE:
         if item in CATALOGUE[category]:
             return True
-    return False 
+    return False
+
 
 # Returns the price of the item if it exists in the Catalogue
 # Returns None otherwise
@@ -31,6 +33,7 @@ def get_price(item):
     for category in CATALOGUE:
         if item in CATALOGUE[category]:
             return CATALOGUE[category][item]
-    return None 
+    return None
+
 
 # maybe just a non nested map since this is a bit overkill
--- solutions/ghost_market/ghost_market.py
+++ solutions/ghost_market/ghost_market.py
@@ -1,9 +1,9 @@
-'''Ghost market shopping spree!!'''
+"""Ghost market shopping spree!!"""
 
 # You do not need to modify this file, though you are certainly
 # welcome to read through it. We hope you enjoy and learn a lot
 # from working on this project.
-#	The CompClub Team
+# 	The CompClub Team
 
 from .constants import CATALOGUE
 
@@ -11,52 +11,53 @@ from .constants import CATALOGUE
 # function definition entirely, or do something else crazy, so
 # we should try to be helpful if that happens.
 try:
-	from .welcome import welcome, get_next_customer, display_catalogue
-	from .shop import add_to_cart, remove_from_cart, show_items
-	from .checkout import checkout
+    from .welcome import welcome, get_next_customer, display_catalogue
+    from .shop import add_to_cart, remove_from_cart, show_items
+    from .checkout import checkout
 except ImportError:
-	# TODO: Print a helpful error message.
-	pass
+    # TODO: Print a helpful error message.
+    pass
 
 
 def main():
-	welcome()
-	customer = get_next_customer()
-	cart = {}		# {'item': quantity}
+    welcome()
+    customer = get_next_customer()
+    cart = {}  # {'item': quantity}
 
-	while True:
-		display_catalogue()
+    while True:
+        display_catalogue()
 
-		# Interpret EOF as quit.
-		try:
-			while True:
-				command = input('/')
-				if command.strip() != '':
-					break
-		except EOFError:
-			break
+        # Interpret EOF as quit.
+        try:
+            while True:
+                command = input("/")
+                if command.strip() != "":
+                    break
+        except EOFError:
+            break
 
-		if command in ('q', 'quit'):
-			break;
-		
-		elif command in ('b', 'buy'):
-			cart = add_to_cart(cart)
+        if command in ("q", "quit"):
+            break
 
-		elif command in ('r', 'remove'):
-			cart = remove_from_cart(cart)
+        elif command in ("b", "buy"):
+            cart = add_to_cart(cart)
 
-		elif command in ('s', 'show'):
-			show_items(cart)
+        elif command in ("r", "remove"):
+            cart = remove_from_cart(cart)
 
-		elif command in ('c', 'checkout'):
-			checkout(customer, cart)
-			customer = get_next_customer()
-			cart = {}
+        elif command in ("s", "show"):
+            show_items(cart)
 
-		else:
-			print('There are a few commands to learn:\n'
-				  '		q, quit - Close the market\n'
-				  '		b, buy - Add an item to cart\n'
-				  '		r, remove - Remove an item from cart\n'
-				  '		c, checkout - Finalise and checkout your order!')
+        elif command in ("c", "checkout"):
+            checkout(customer, cart)
+            customer = get_next_customer()
+            cart = {}
 
+        else:
+            print(
+                "There are a few commands to learn:\n"
+                "		q, quit - Close the market\n"
+                "		b, buy - Add an item to cart\n"
+                "		r, remove - Remove an item from cart\n"
+                "		c, checkout - Finalise and checkout your order!"
+            )
